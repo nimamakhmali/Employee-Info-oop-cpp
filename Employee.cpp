d683a14ef65c2cda089791d0c047d7ac87dd8db7
nimaMakhmali <nmkhmly790@gmail.com>
2024-03-24 20:35:40 +0330

    added class

--- Employee.cpp
+++ Employee.cpp
@@ -1,3 +1,14 @@
+//**************************************************
+//     Nima Makhmali  -----> Buali sina university
+//**************************************************
 #include <iostream>
+ #include <string>
+ using namespace std;
 
-using namespace std;
+ class Employee
+ {
+  private:
+    int employeeId;
+    string employeeName;
+  public:
+ };

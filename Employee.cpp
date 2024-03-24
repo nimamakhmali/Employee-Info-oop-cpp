0ce89fa7c7603658aeee7caa1a2b6b8c4588bb5a
nimaMakhmali <nmkhmly790@gmail.com>
2024-03-24 22:01:30 +0330

    complete class

--- Employee.cpp
+++ Employee.cpp
@@ -11,4 +11,20 @@
     int employeeId;
     string employeeName;
   public:
+    void inputBasicInfo()
+    {
+      cout<<" INPUT EMPLOYEE ID : ";
+      cin>>employeeId;
+      cout<<" INPUT EMPLOYEE NAME : ";
+      cin.ignore();
+      getline(cin,employeeName); 
+    }
+    void displayInfo()const
+    {
+    cout<<" ----------INFORMATION---------- ";
+    cout<<"Employee Name : "<<employeeName<<endl;
+    cout<<"Employee ID : "<<employeeId<<endl;
+    }
+    virtual void inputInfo()=0;
+    virtual void displayInfo()const=0;
  };
